8aa705ff84fe5890e5693c8db7a3d0ab46ff2cb0
Michael Ferger <mferger92@gmail.com>
2016-04-12 12:06:45 -0600

    rating star done

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,6 +1,14 @@
 $(function() {
+  //Rating
+  $(".rating input:radio").attr("checked", false);
+    $('.rating input').click(function () {
+        $(".rating span").removeClass('checked');
+        $(this).parent().addClass('checked');
+    });
 
-
-
-
+    $('input:radio').change(
+    function(){
+      //userRating for value
+        var userRating = this.value;
+    });
 });
