601d84630c11ee5156e5bd1616249e3b33532bcf
Phil Skaggs <prskaggsjr@gmail.com>
2016-04-11 20:58:56 -0600

    Created new get call for new posting view in user route

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/post', function(req, res, next){
+  res.render('newposting', { title: 'Lact2Go' });
+})
+
 module.exports = router;
