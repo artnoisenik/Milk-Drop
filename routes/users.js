0a563ed747a60dcbf06157ea71fb6ae0bdb39d8c
Phil Skaggs <prskaggsjr@gmail.com>
2016-04-11 22:05:30 -0600

    created get and post call for editing posting

--- routes/users.js
+++ routes/users.js
@@ -27,4 +27,30 @@ router.post('/post', function(req, res, next){
   });
 })
 
+router.get('/post/edit/:id', function(req, res, next){
+  knex('listings')
+  .where('id', req.params.id).first()
+  .then(function(post){
+    console.log(post);
+    res.render('editposting', {title: 'Lact2Go', post:post})
+
+  })
+})
+
+router.post('/post/edit/:id', function(req, res, next){
+  knex('listings')
+    .where('id', req.params.id).first()
+    .returning('id')
+    .update({
+    'title':req.body.title,
+    'post_end':req.body.post_end,
+    'amount':req.body.amount,
+    'cost_per_ounce':req.body.cost_per_ounce,
+    'description':req.body.description
+  })
+  .then(function(post_id) {
+    res.redirect(302, '/users/post/edit/' + post_id);
+  });
+})
+
 module.exports = router;
