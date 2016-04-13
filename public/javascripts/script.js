7c2b412aaf83e44818dca450f36d482e92f53332
Michael Ferger <mferger92@gmail.com>
2016-04-13 14:26:16 -0600

    added client side validation

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -14,5 +14,4 @@ $(function() {
   if ($('.ratings').hasClass('5')) {
     $('.5').html('<span class="glyphicon glyphicon-star" aria-hidden="true"></span><span class="glyphicon glyphicon-star" aria-hidden="true"></span><span class="glyphicon glyphicon-star" aria-hidden="true"></span><span class="glyphicon glyphicon-star" aria-hidden="true"></span><span class="glyphicon glyphicon-star" aria-hidden="true"></span>');
   }
-
 });
