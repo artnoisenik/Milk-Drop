486358a32f669a091cb47ddeeb3e51d95240c116
Phil Skaggs <prskaggsjr@gmail.com>
2016-04-13 10:10:22 -0600

    updated signup post call with verification

--- routes/index.js
+++ routes/index.js
@@ -42,9 +42,22 @@ router.get('/signup', function(req, res, next) {
 });
 
 router.post('/signupSubmit', function(req, res, next) {
-  res.render('completeprofile', {
-    user: req.body
-  });
+  var errorArray = [];
+
+  if(!req.body.username) {
+    errorArray.push('Please enter a username');
+  }
+  if(!req.body.password) {
+    errorArray.push('Please enter a password');
+  }
+  if(errorArray.length > 0) {
+    res.render('signup', {errors: errorArray});
+  }
+  else{
+    res.render('completeprofile', {
+      user: req.body
+    });
+  }
 });
 
 router.post('/signupSubmit2', function(req, res, next) {
@@ -71,6 +84,49 @@ router.post('/login', function(req, res, next) {
       res.redirect('/signup');
     }
   });
+});
+
+router.post('/signup', function(req,res,next){
+  // validate that the form was filled out
+   var errorArray = [];
+
+   if(!req.body.username) {
+     errorArray.push('Please enter a username');
+   }
+   if(!req.body.password) {
+     errorArray.push('Please enter a password');
+   }
+   if(errorArray.length > 0) {
+     res.render('signUp', {errors: errorArray});
+   }
+   else{
+  var hash = bcrypt.hashSync(req.body.password, 8);
+  knex('users')
+  .insert({'username': req.body.username, 'password': hash})
+  .then(function(response){
+    res.redirect('/');
+  })
+}
+
+});
+
+router.post('/login', function(req,res,next){
+  knex('users')
+  .where('username', '=', req.body.username)
+  .first()
+  .then(function(response){
+    if(response && bcrypt.compareSync(req.body.password, response.password)){
+      req.session.user = response.username;
+      res.redirect('/');
+    } else {
+      res.render('login', {error: 'Invalid username or password'});
+    }
+  });
+});
+
+router.get('/logout', function(req,res,next){
+  req.session.user = null;
+  res.redirect('/');
 });
 
 
