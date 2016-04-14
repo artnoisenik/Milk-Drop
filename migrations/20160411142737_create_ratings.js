e2f1706030264e954c98d0f75ed123a0b12147be
Brad Butterfield <butters5789@gmail.com>
2016-04-13 19:33:03 -0600

    admin dashboard

--- migrations/20160411142737_create_ratings.js
+++ migrations/20160411142737_create_ratings.js
@@ -2,8 +2,8 @@ exports.up = function(knex, Promise) {
   return knex.schema.createTable('ratings', function(table) {
     table.increments('id');
     table.integer('reciever_id').unsigned().notNullable().references('id').inTable('users').onDelete('cascade').onUpdate('cascade');
-    table.integer('giver_id').unsigned().references('id').inTable('users').onDelete('cascade').onUpdate('cascade');
-    table.integer('rating').notNullable().defaultTo(0);
+    table.integer('giver_id').unsigned().notNullable().defaultTo(0);
+    table.integer('rating').unsigned().notNullable().defaultTo(0);
   });
 };
 
